e5e21a9b55db77ece4a0992edb14684f499facc3
Svetlana Zharkova <svetazhar7@gmail.com>
2023-02-20 10:13:43 +0300

    upd

--- user_bot.py
+++ user_bot.py
@@ -38,6 +38,7 @@
     elif (check("level") == 4):
         if (check("gold", x, y) != 0):
             return "take"
+        """
         if (check("player", 3, 16)):
             return "right"
         if (check("player", 4, 16)):
@@ -45,6 +46,15 @@
         if (check("player", 10, 13)):
             return "up"
         if (check("player", 10, 12)):
+            return "up"
+        """
+        if (x==3 and y==16):
+            return "right"
+        if (x==4 and y==16):
+            return "right"
+        if (x==10 and y==13):
+            return "up"
+        if (x==10 and y==12):
             return "up"
         if (check("wall", x-1, y) == True and check("wall", x, y-1) == False):
             return "up"
@@ -62,19 +72,54 @@
             return "left"
         if (check("wall", x+1, y+1) == True and check("wall", x,y+1) == False):
             return "down"
+
+
     elif (check("level") == 5):
         if (check("gold", x, y) != 0):
             return "take"
-        if (check("wall", x-1, y - 1) ==  True and check("wall", x-2, y - 1) ==  True  and  check("wall", x-1, y) ==  False):
-            return "left"
-        if (check("wall", x + 1, y) ==  False):
+        if (check("gold",x+1,y)!=0):
             return "right"
-        if (check("wall", x, y + 1) == False):
-            return "down"
-        if (check("wall", x - 1, y) == False):
+
+        else:
+            if (check("gold",x,y+1)!=0):
+                return "down"
+            else:
+                if (check("gold", x-1, y) != 0):
+                    return "left"
+                else:
+                    if (check("gold", x, y - 1) != 0):
+                        return "up"
+        if ( y == 15 and (check("wall",x,y-1)==False)):
             return "left"
-        if (check("wall", x, y - 1) ==  False):
-            return "up"
+        if (check("wall",x+1,y)==False):
+            return "right"
+        else:
+            if (check("wall",x,y+1)==False):
+                return "down"
+            else:
+                if (check("wall",x-1,y)==False):
+                    return "left"
+                else:
+                    return "up"
+
+
+       # if (check("wall", x + 1, y) == True and check("wall", x, y-1) == True and check("wall", x + 1, y-1) == True and check("wall", x + 1, y+1) == False and check("wall", x + 1, y-2) == True):
+          #  return "down"
+       # if (check("wall", x + 1, y) == False):
+        #    return "right"
+       # if (check("wall", x + 1, y) == False):
+       #     return "right"
+       # if (check("wall", x, y+1) == False):
+        #    return "down"
+       # if (check("wall", x+1, y) == False and check("wall", x+2, y) == False and check("wall", x+1, y+1) == False):
+      #      return "right"
+       # if (check("wall", x, y+1) == False  and check("wall", x, y+2) == False and check("wall", x-1, y+1) == False):
+       #     return "down"
+      #  if (check("wall", x-1, y) == False  and check("wall", x-2, y) == False and check("wall", x-1, y-1) == False):
+      #      return "left"
+     #   if (check("wall", x, y-1) == False  and check("wall", x, y-2) == False and check("wall", x+1, y-1) == False):
+     #       return "up"
+
 
 
 
